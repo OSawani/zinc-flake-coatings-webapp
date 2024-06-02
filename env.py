183314c4d9f263b23f27decc59f183b333eae816
sawaomar <omarsawani@gmail.com>
2024-06-02 18:58:20 +0200

    fix: removes env.py from tracking

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault('DATABASE_URL', 'postgres://postgres.artflkcafnecayzoaych:791xdrv6aS4jDkHa@aws-0-eu-central-1.pooler.supabase.com:5432/postgres')
-os.environ.setdefault('SECRET_KEY', 'di-d-e3&bg&=hr13na_rh8v=r&t*(g+=_8eg%c%1iy#55@_2i^c7!')
