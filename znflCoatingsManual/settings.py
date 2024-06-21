c991745208d1174721a6d5649cb875c8339e0332
sawaomar <omarsawani@gmail.com>
2024-06-21 15:33:03 +0200

    fix: sets pool connection options for supabase DB to allow for longer connections

--- znflCoatingsManual/settings.py
+++ znflCoatingsManual/settings.py
@@ -142,7 +142,11 @@ WSGI_APPLICATION = 'znflCoatingsManual.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # Custom user model setting
